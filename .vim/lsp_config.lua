e04b68b231229c1bdd3d08a602c5d884886bbda7
Moshe Avni <moshe@avni.us>
2021-06-05 23:02:36 +0300

    wip

--- .vim/lsp_config.lua
+++ .vim/lsp_config.lua
@@ -1,11 +1,10 @@
 lspconfig = require'lspconfig'
-completion_callback = require'completion'.on_attach
 
 -- language servers
 lspconfig.pyls.setup{}
 lspconfig.tsserver.setup{}
 
 -- completion
-lspconfig.pyls.setup{on_attach=completion_callback}
-lspconfig.tsserver.setup{on_attach=completion_callback}
-lspconfig.rust_analyzer.setup{on_attach=completion_callback}
+lspconfig.pyls.setup{}
+lspconfig.tsserver.setup{}
+lspconfig.rust_analyzer.setup{}
