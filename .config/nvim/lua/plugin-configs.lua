a49155e8718781657944fdffdcf524c73c3ef346
Moshe Avni <mavni@netapp.com>
2022-05-03 00:42:03 +0300

    Require impatient before

--- .config/nvim/lua/plugin-configs.lua
+++ .config/nvim/lua/plugin-configs.lua
@@ -1,5 +1,3 @@
-require('impatient')
-
 require('neoscroll').setup({
     -- All these keys will be mapped to their corresponding default scrolling animation
     mappings = {'<C-u>', '<C-d>', 'zt', 'zz', 'zb'}
