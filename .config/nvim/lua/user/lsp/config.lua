5043f3cb8e77751c07efd02a15211a3df3965676
Moshe Avni <mavni@netapp.com>
2022-05-25 11:35:41 +0300

    fix lsp diagnostics and fix some stuff

--- .config/nvim/lua/user/lsp/config.lua
+++ .config/nvim/lua/user/lsp/config.lua
@@ -1,7 +1,8 @@
-local default_on_attach = require 'user.lsp.on-attach'
+local on_attaches = require 'user.lsp.on-attach'
+local lsp_status = require 'lsp-status'
+local default_on_attach = on_attaches.default
+local minimal_on_attach = on_attaches.minimal
 require 'user.lsp.null-ls'
--- require('lsp_signature').setup {}
-local util = require 'lspconfig.util'
 local lsp_installer = require 'nvim-lsp-installer.servers'
 
 -- Set formatting of lsp log
@@ -9,53 +10,66 @@ require('vim.lsp.log').set_format_func(vim.inspect)
 
 local function ensure_server(name)
   local _, server = lsp_installer.get_server(name)
+  ---@diagnostic disable-next-line: undefined-field
   if not server:is_installed() then
+    ---@diagnostic disable-next-line: undefined-field
     server:install()
   end
   return server
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+if lsp_status then
+  capabilities = vim.tbl_deep_extend('keep', capabilities, lsp_status.capabilities)
+end
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- ansiblels
+---@diagnostic disable-next-line: undefined-field
 ensure_server('ansiblels'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- ansblel
+---@diagnostic disable-next-line: undefined-field
 ensure_server('awk_ls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- bashls
+---@diagnostic disable-next-line: undefined-field
 ensure_server('bashls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- dockerls
+---@diagnostic disable-next-line: undefined-field
 ensure_server('dockerls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- eslint
+---@diagnostic disable-next-line: undefined-field
 ensure_server('eslint'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- groovyls
+---@diagnostic disable-next-line: undefined-field
 ensure_server('groovyls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- html
+---@diagnostic disable-next-line: undefined-field
 ensure_server('html'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- json
 local jsonls = ensure_server 'jsonls'
+---@diagnostic disable-next-line: undefined-field
 jsonls:setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
@@ -69,6 +83,7 @@ jsonls:setup {
   },
 }
 -- python
+---@diagnostic disable-next-line: undefined-field
 ensure_server('pyright'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
@@ -86,35 +101,45 @@ local luadev = require('lua-dev').setup {
     on_attach = default_on_attach,
   },
 }
+---@diagnostic disable-next-line: undefined-field
 ensure_server('sumneko_lua'):setup(luadev)
 --terraformls
+---@diagnostic disable-next-line: undefined-field
 ensure_server('terraformls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
-  root_dir = util.root_pattern '.terraform',
+  cmd = {
+    "terraform-ls",
+    "serve",
+    "-log-file=/tmp/terraform-ls-{{pid}}.log",
+    [[-tf-log-file='/tmp/terraform-exec-1-{{args}}.log']]
+  },
 }
 --tsserver
+---@diagnostic disable-next-line: undefined-field
 ensure_server('tsserver'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 
 --vimls
+---@diagnostic disable-next-line: undefined-field
 ensure_server('vimls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 
 --jdtls
+---@diagnostic disable-next-line: undefined-field
 ensure_server('jdtls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
 }
 -- yaml
 local clock = os.clock
-yaml_install_path = vim.fn.expand '~' .. '/Repos/yaml-language-server'
+local yaml_install_path = vim.fn.expand '~' .. '/Repos/yaml-language-server'
 if vim.fn.empty(vim.fn.glob(yaml_install_path)) > 0 then
-  function sleep(n) -- seconds
+  local function sleep(n) -- seconds
     local t0 = clock()
     while clock() - t0 <= n do
     end
@@ -124,6 +149,7 @@ if vim.fn.empty(vim.fn.glob(yaml_install_path)) > 0 then
   sleep(5)
   vim.fn.execute('!cd ' .. yaml_install_path .. ' && yarn run build')
 end
+---@diagnostic disable-next-line: undefined-field
 ensure_server('yamlls'):setup {
   on_attach = default_on_attach,
   capabilities = capabilities,
@@ -176,5 +202,3 @@ for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
-return luadev
