5d48aff1062c262c5de8736dc3fac69bf7534768
Moshe Avni <mavni@netapp.com>
2022-05-26 10:49:49 +0300

    set maps on buffer level

--- .config/nvim/lua/user/lsp/maps.lua
+++ .config/nvim/lua/user/lsp/maps.lua
@@ -1,37 +1,48 @@
 local utils = require 'user.utils'
 local opts = utils.map_opts
-local keymap = utils.keymap
 
-return function()
+return function(bufnr)
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
   -- Goto previous/next diagnostic warning/error
   -- Use `[g` and `]g` to navigate diagnostics
-  keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts.silent)
-  keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts.silent)
+  buf_set_keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts.silent)
+  buf_set_keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts.silent)
 
   -- GoTo code navigation
-  keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts.silent)
-  keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts.silent)
-  keymap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts.silent)
-  keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts.silent)
-  keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references({ includeDeclaration = false })<CR>', opts.silent)
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts.silent)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts.silent)
+  buf_set_keymap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts.silent)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts.silent)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references({ includeDeclaration = false })<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>lrf', '<cmd>lua vim.lsp.buf.references()<CR>', opts.silent)
 
   -- Documentation
-  keymap('i', '<M-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts.silent)
-  keymap('n', '<leader>lk', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts.silent)
+  buf_set_keymap('i', '<M-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>lk', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts.silent)
   -- calling twice make the cursor go into the float window. good for navigating big docs
-  keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts.silent)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts.silent)
 
   -- Refactor rename
-  keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>lrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts.silent)
+
+  -- Workspace
+  buf_set_keymap('n', '<leader>law', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>lrw', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>llw', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts.silent)
+
+  -- Diagnostics
+  buf_set_keymap('n', '<leader>lq', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>ld', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>le', '<cmd>lua vim.diagnostic.open_float()<cr>', opts.silent)
 
   -- Code action
-  keymap('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
-  keymap('n', '<leader>lx', '<cmd>lua vim.lsp.codelens.run()<CR>', {})
-  keymap('x', '<leader>la', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', {})
-  keymap('n', '<leader>lq', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts.silent)
-  keymap('n', '<leader>lp', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts.silent)
+  buf_set_keymap('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
+  buf_set_keymap('n', '<leader>lx', '<cmd>lua vim.lsp.codelens.run()<CR>', {})
+  buf_set_keymap('x', '<leader>la', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', {})
+  buf_set_keymap('n', '<leader>lp', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts.silent)
   -- vim.keymap.set('n', '<leader>lp', function()
   --   utils.lsp_formatting(0)
   -- end, { silent = true })
-  keymap('n', '<leader>le', '<cmd>lua vim.diagnostic.open_float()<cr>', opts.silent)
 end
