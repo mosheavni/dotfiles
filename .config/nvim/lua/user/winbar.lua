dcdb6b68dc5905a2c13311533026e0f61a0e3485
Moshe Avni <mavni@netapp.com>
2022-05-29 21:03:07 +0300

    winbar wip

--- .config/nvim/lua/user/winbar.lua
+++ .config/nvim/lua/user/winbar.lua
@@ -5,11 +5,11 @@ vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#dedede', fg = '#ff3838' })
 
 function M.eval()
     local file_path = vim.api.nvim_eval_statusline('%f', {}).str
-    local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and '⊚' or ''
+    local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and ' [+]' or ''
 
     file_path = file_path:gsub('/', ' ➤ ')
 
-    return '%#WinBarPath#'
+    return '%=%#WinBarPath#'
      .. file_path
      .. '%*'
      .. '%#WinBarModified#'
