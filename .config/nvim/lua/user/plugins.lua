3c265d2ccb1b2fa307ad79a6d201a28b0dfb927e
Moshe Avni <mavni@netapp.com>
2022-09-27 10:39:56 +0300

    remove debugprint

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -168,7 +168,6 @@ return packer.startup(function(use)
     'rcarriga/cmp-dap',
     'Pocco81/dap-buddy.nvim',
   }
-  use 'andrewferrier/debugprint.nvim'
 
   -- Functionality Tools
   use 'kevinhwang91/nvim-hlslens'
