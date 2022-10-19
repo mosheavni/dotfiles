b06bf733c6e50e03fa6680db65bdf23b72ff1e1a
Moshe Avni <mavni@netapp.com>
2022-10-19 14:37:17 +0300

    remove playground TS

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -47,24 +47,6 @@ configs.setup {
     'vim',
     'yaml',
   },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  },
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -77,9 +59,6 @@ configs.setup {
   markid = { enable = true },
   highlight = {
     enable = true,
-    disable = {
-      'yaml',
-    },
     additional_vim_regex_highlighting = false,
   },
   indent = {
