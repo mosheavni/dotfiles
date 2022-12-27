f622383267816d3b7d66c6ffba853e6e5666f89f
Moshe Avni <mavni@netapp.com>
2022-12-27 20:00:17 +0200

    textobjects jumps

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -83,6 +83,26 @@ configs.setup {
         ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
       },
     },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
   },
   refactor = {
     highlight_current_scope = { enable = true },
