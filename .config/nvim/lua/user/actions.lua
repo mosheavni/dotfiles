7b5ad3628bbbee9740233d0a51597e63ba62f1e7
Moshe Avni <mavni@netapp.com>
2023-01-09 13:48:21 +0200

    random stuff and actions menu

--- .config/nvim/lua/user/actions.lua
+++ .config/nvim/lua/user/actions.lua
@@ -1,4 +1,21 @@
 local utils = require 'user.utils'
+local pretty_print = utils.pretty_print
+local find_in_project = function(bang)
+  vim.ui.input({ prompt = 'Enter search term (blank for word under cursor): ' }, function(search_term)
+    if search_term then
+      search_term = ' ' .. search_term
+    end
+
+    local bang_str = bang and '!' or ''
+
+    vim.cmd('RipGrepCWORD' .. bang_str .. search_term)
+  end)
+end
+
+local T = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
 local M = {}
 
 M.pretty_print = function(message)
@@ -181,6 +198,7 @@ M.git = {
     vim.cmd 'G reset'
   end,
 }
+
 M.lsp = {
   ['Format'] = function()
     require('user.lsp.formatting').format()
@@ -215,6 +233,141 @@ M.lsp = {
   end,
   ['Diagnostics quickfix list'] = function()
     vim.diagnostic.setqflist()
+  end,
+}
+
+M.random = {
+  ['Find in pwd (literal search)'] = function()
+    find_in_project(true)
+  end,
+  ['Find in pwd (regex search)'] = function()
+    find_in_project(false)
+  end,
+  ['Replace word under cursor'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'r')
+  end,
+  ['Select all'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'sa')
+  end,
+  ['Indent block forward'] = function()
+    vim.fn.feedkeys(T '<leader>gt')
+  end,
+  ['Duplicate / Copy number of lines'] = function()
+    vim.ui.input({ prompt = 'Enter how many lines down: ' }, function(lines_down)
+      if not lines_down then
+        lines_down = ''
+      end
+      vim.fn.feedkeys(lines_down .. T '<leader>' .. 'cp')
+    end)
+  end,
+  ['Center Focus'] = function()
+    vim.fn.feedkeys 'zz'
+  end,
+  ['Bottom Focus'] = function()
+    vim.fn.feedkeys 'zb'
+  end,
+  ['Top Focus'] = function()
+    vim.fn.feedkeys 'zt'
+  end,
+  ['Record Macro'] = function()
+    vim.ui.input({ prompt = 'Macro letter: ' }, function(macro_letter)
+      if not macro_letter then
+        macro_letter = 'q'
+      end
+      vim.fn.feedkeys('q' .. macro_letter)
+      pretty_print('Recording macro ' .. macro_letter .. ' (hit q when finished)', 'Macro')
+      pretty_print('You can repeat this macro with @' .. macro_letter, 'Macro')
+    end)
+  end,
+
+  ['Repeat Macro'] = function()
+    vim.ui.input({ prompt = 'Macro letter: ' }, function(macro_letter)
+      if not macro_letter then
+        macro_letter = 'q'
+      end
+      vim.ui.input({ prompt = 'Enter how many times (leave blank for once): ' }, function(macro_times)
+        if not macro_times then
+          macro_times = ''
+        end
+        vim.fn.feedkeys(macro_times .. '@' .. macro_letter)
+      end)
+    end)
+  end,
+  ['Toggle Terminal'] = function()
+    vim.fn.feedkeys(T '<F6>')
+  end,
+  ['Create a new terminal window'] = function()
+    vim.fn.feedkeys(T '<F7>')
+  end,
+  ['Move to next terminal window'] = function()
+    vim.fn.feedkeys(T '<F8>')
+  end,
+  ['Find files'] = function()
+    vim.fn.feedkeys(T '<C-p>')
+  end,
+  ['Find buffers'] = function()
+    vim.fn.feedkeys(T '<C-b>')
+  end,
+  ['Open Nvim Tree File Browser'] = function()
+    vim.fn.feedkeys(T '<C-o>')
+  end,
+  ['Close all notifications'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'x')
+  end,
+  ['Quit all'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'qq')
+  end,
+  ['Paste from clipboard'] = function()
+    vim.fn.feedkeys(T '<C-v>')
+  end,
+  ['Copy entire file to clipboard'] = function()
+    vim.fn.feedkeys 'Y'
+  end,
+  ['Convert \\n to new lines'] = function()
+    vim.fn.feedkeys(T '<leader>' .. T '<cr>')
+  end,
+  ['Move line down'] = function()
+    vim.fn.feedkeys '-'
+  end,
+  ['Move line up'] = function()
+    vim.fn.feedkeys '_'
+  end,
+  ['Copy full file path to clipboard'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'cfa')
+  end,
+  ['Copy relative file path to clipboard'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'cfp')
+  end,
+  ['Copy directory path to clipboard'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'cfd')
+  end,
+  ['Split long bash line'] = function()
+    vim.fn.feedkeys(T '<leader>' .. [[\]])
+  end,
+  ['Delete all hidden buffers'] = function()
+    vim.cmd 'BDelete hidden'
+  end,
+  ['Delete current buffer'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'bd')
+  end,
+  ['Yaml to Json'] = function()
+    vim.cmd.Yaml2Json()
+  end,
+  ['Json to Yaml'] = function()
+    vim.cmd.Json2Yaml()
+  end,
+  ['Change indent size'] = function()
+    vim.cmd.Json2Yaml()
+    vim.fn.feedkeys 'cii'
+  end,
+  ['Convert tabs to spaces'] = function()
+    local original_expandtab = vim.opt_global.expandtab:get()
+    vim.opt.expandtab = true
+    vim.cmd.retab()
+    vim.opt.expandtab = original_expandtab
+  end,
+  ['Diff unsaved with saved file'] = function()
+    vim.fn.feedkeys(T '<leader>' .. 'ds')
   end,
 }
 
