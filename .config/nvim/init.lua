57f31f94c9335911f36fbe933b0dc4f46dacb9f7
Moshe Avni <mavni@netapp.com>
2023-01-27 22:35:29 +0200

    align autocmds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,10 +4,3 @@ require 'user.options'
 require 'user.mappings'
 require('lazy').setup('plugins', require('user.lazy').config)
 require 'user.autocommands'
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'VeryLazy',
-  callback = function()
-    require('user.menu').setup()
-  end,
-})
