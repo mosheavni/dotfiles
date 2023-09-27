194e49a3d74f8c4b74ad8278a958f33e212914c0
Moshe Avni <mavni@netapp.com>
2023-09-27 15:42:51 +0300

    move functionality

---
+++ .config/nvim/lua/plugins/functionality.lua
@@ -0,0 +1,146 @@
+local utils = require 'user.utils'
+local nnoremap = utils.nnoremap
+local tnoremap = utils.tnoremap
+
+local M = {
+  -------------------------
+  -- Functionality Tools --
+  -------------------------
+  {
+    'monkoose/matchparen.nvim',
+    keys = { '%' },
+    config = true,
+  },
+  {
+    'kiran94/s3edit.nvim',
+    cmd = 'S3Edit',
+    config = true,
+  },
+  {
+    'voldikss/vim-floaterm',
+    keys = { 'F6', 'F7', 'F8' },
+    cmd = {
+      'FloatermFirst',
+      'FloatermHide',
+      'FloatermKill',
+      'FloatermLast',
+      'FloatermNew',
+      'FloatermNext',
+      'FloatermPrev',
+      'FloatermSend',
+      'FloatermShow',
+      'FloatermToggle',
+      'FloatermUpdate',
+    },
+    init = function()
+      nnoremap('<F6>', '<Cmd>FloatermToggle<CR>', true)
+      nnoremap('<F7>', '<Cmd>FloatermNew<CR>', true)
+      nnoremap('<F8>', '<Cmd>FloatermNext<CR>', true)
+      vim.g['floaterm_height'] = 0.9
+      vim.g['floaterm_keymap_new'] = '<F7>'
+      vim.g['floaterm_keymap_next'] = '<F8>'
+      vim.g['floaterm_keymap_toggle'] = '<F6>'
+      vim.g['floaterm_width'] = 0.7
+    end,
+  },
+  -- {
+  --   'samjwill/nvim-unception',
+  --   event = 'VeryLazy',
+  -- },
+  {
+    'mosheavni/vim-dirdiff',
+    cmd = { 'DirDiff' },
+  },
+  {
+    'simeji/winresizer',
+    keys = { '<C-e>' },
+    config = function()
+      vim.g.winresizer_vert_resize = 4
+      vim.g.winresizer_start_key = '<C-E>'
+      tnoremap('<C-E>', '<Esc><Cmd>WinResizerStartResize<CR>', true)
+    end,
+  },
+  {
+    'pechorin/any-jump.vim',
+    cmd = { 'AnyJump', 'AnyJumpVisual' },
+    keys = { '<leader>j' },
+    config = function()
+      nnoremap('<leader>j', '<cmd>AnyJump<CR>')
+    end,
+  },
+  {
+    'kazhala/close-buffers.nvim',
+    config = true,
+    cmd = { 'BDelete', 'BWipeout' },
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    build = 'cd app && yarn install',
+    config = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    cmd = 'MarkdownPreview',
+    ft = 'markdown',
+  },
+  {
+    'max397574/better-escape.nvim',
+    opts = {
+      mapping = { 'jk' },
+    },
+    event = 'InsertEnter',
+  },
+  {
+    'AndrewRadev/linediff.vim',
+    cmd = { 'Linediff' },
+  },
+  {
+    'nvim-pack/nvim-spectre',
+    keys = { { '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', desc = 'Start Spectre' } },
+  },
+  {
+    'ellisonleao/carbon-now.nvim',
+    lazy = true,
+    cmd = 'CarbonNow',
+    opts = { open_cmd = 'open' },
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    cmd = { 'Oil' },
+  },
+  {
+    'rmagatti/gx-extended.nvim',
+    opts = {
+      log_level = 'debug',
+      open_fn = require('lazy.util').open,
+      extensions = {
+        { -- match github repos in lazy.nvim plugin specs
+          patterns = { '*/plugins/**/*.lua' },
+          name = 'neovim plugins',
+          match_to_url = function(line_string)
+            local line = string.match(line_string, '["|\'].*/.*["|\']')
+            local repo = vim.split(line, ':')[1]:gsub('["|\']', '')
+            local url = 'https://github.com/' .. repo
+            return line and repo and url or nil
+          end,
+        },
+      },
+    },
+    -- keys = { 'gx' },
+  },
+  -- {
+  --   'sontungexpt/url-open',
+  --   event = 'VeryLazy',
+  --   cmd = 'URLOpenUnderCursor',
+  --   keys = { { 'gx', '<esc>:URLOpenUnderCursor<cr>', desc = 'Open URL under cursor' } },
+  --   config = function()
+  --     local status_ok, url_open = pcall(require, 'url-open')
+  --     if not status_ok then
+  --       return
+  --     end
+  --     url_open.setup {}
+  --   end,
+  -- },
+}
+
+return M
