5147e08fd557cbbaacdb6588aa89d183820a70e8
Moshe Avni <mavni@netapp.com>
2023-10-02 14:12:49 +0300

    align search next

--- .config/nvim/lua/plugins/hlslens.lua
+++ .config/nvim/lua/plugins/hlslens.lua
@@ -1,8 +1,8 @@
 return {
   'kevinhwang91/nvim-hlslens',
   keys = {
-    { 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]] },
-    { 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+    { 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>zz]] },
+    { 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>zz]] },
     { '*', [[*<Cmd>lua require('hlslens').start()<CR>]] },
     { '#', [[#<Cmd>lua require('hlslens').start()<CR>]] },
     { 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]] },
