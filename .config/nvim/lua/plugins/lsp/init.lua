2cd95e4879122505d538c7edda4a39e917eda910
Moshe Avni <mavni@netapp.com>
2023-12-17 17:13:28 +0200

    fix diagnostics signs on nightly https://github.com/neovim/neovim/issues/26619

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -136,20 +136,6 @@ M.config = function(_, opts)
   -- Set formatting of lsp log
   require('vim.lsp.log').set_format_func(vim.inspect)
 
-  -- general LSP config
-  -- show icons in the sidebar
-  local signs = {
-    Error = '',
-    Warn = ' ',
-    Hint = ' ',
-    Info = ' ',
-  }
-
-  for type, icon in pairs(signs) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
-
   local servers = require 'user.lsp.servers'
   ------------------
   -- Capabilities --
@@ -165,11 +151,16 @@ M.config = function(_, opts)
   -----------------
   -- Diagnostics --
   -----------------
+  -- show icons in the sidebar
+  local signs = {
+    [vim.diagnostic.severity.ERROR] = '',
+    [vim.diagnostic.severity.WARN] = ' ',
+    [vim.diagnostic.severity.HINT] = ' ',
+    [vim.diagnostic.severity.INFO] = ' ',
+  }
   vim.diagnostic.config {
+    signs = { text = signs },
     update_in_insert = false,
-    -- underline = {
-    --   severity = { max = vim.diagnostic.severity.INFO },
-    -- },
     virtual_text = {
       severity = { min = vim.diagnostic.severity.WARN },
     },
