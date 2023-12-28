c2b0628154826fa2af41a08d6e49250c8925cbd0
Moshe Avni <mavni@netapp.com>
2023-12-28 20:12:46 +0200

    add db actions

--- .config/nvim/lua/plugins/databases.lua
+++ .config/nvim/lua/plugins/databases.lua
@@ -1,7 +1,43 @@
 local M = {
   {
     'tpope/vim-dadbod',
-    cmd = { 'DB', 'DBUI' },
+    cmd = {
+      'DB',
+      'DBUI',
+      'DBUIClose',
+      'DBUIToggle',
+      'DBUIFindBuffer',
+      'DBUIRenameBuffer',
+      'DBUIAddConnection',
+      'DBUILastQueryInfo',
+      'DBUIHideNotifications',
+      'DBCompletionClearCache',
+    },
+    init = function()
+      require('user.menu').add_actions('Database', {
+        ['Add Connection'] = function()
+          vim.cmd 'DBUIAddConnection'
+        end,
+        ['Rename Buffer'] = function()
+          vim.cmd 'DBUIRenameBuffer'
+        end,
+        ['Find Buffer'] = function()
+          vim.cmd 'DBUIFindBuffer'
+        end,
+        ['Last Query Info'] = function()
+          vim.cmd 'DBUILastQueryInfo'
+        end,
+        ['Toggle UI'] = function()
+          vim.cmd 'DBUIToggle'
+        end,
+        ['Close UI'] = function()
+          vim.cmd 'DBUIClose'
+        end,
+        ['Hide UI Notifications'] = function()
+          vim.cmd 'DBUIHideNotifications'
+        end,
+      })
+    end,
     dependencies = {
       {
         'kristijanhusak/vim-dadbod-ui',
