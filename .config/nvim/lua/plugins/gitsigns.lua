c028f56c2f9c017f17d8c8bf4626fa690207c446
Moshe Avni <mavni@netapp.com>
2024-04-18 10:16:17 +0300

    replace gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,51 +1,21 @@
 local M = {
-  'lewis6991/gitsigns.nvim',
+  'echasnovski/mini.diff',
+  version = false,
   event = 'BufReadPre',
-  cmd = { 'Gitsigns' },
   opts = {
-    on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
-
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
-
-      -- Navigation
-      map('n', ']c', function()
-        if vim.wo.diff then
-          return ']c'
-        end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true })
-
-      map('n', '[c', function()
-        if vim.wo.diff then
-          return '[c'
-        end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true })
-
-      -- Actions
-      map('n', '<leader>hp', gs.preview_hunk)
-      map('n', '<leader>hb', gs.toggle_current_line_blame)
-      map('n', '<leader>hd', gs.toggle_deleted)
-
-      -- Text object
-      map({ 'o', 'x' }, 'ih', '<cmd>Gitsigns select_hunk<CR>')
-    end,
+    view = {
+      style = 'number',
+      signs = {
+        add = '┃',
+        change = '┃',
+        delete = '▁',
+      },
+    },
   },
   init = function()
     require('user.menu').add_actions('Git', {
-      ['Preview hunk (<leader>hp)'] = function()
-        vim.cmd.Gitsigns 'preview_hunk'
+      ['Toggle signs (mini.diff)'] = function()
+        MiniDiff.toggle(0)
       end,
       ['Toggle current line blame (<leader>hb)'] = function()
         vim.cmd.Gitsigns 'toggle_current_line_blame'
