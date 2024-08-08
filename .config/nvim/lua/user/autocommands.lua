cdf88e434891d75ec813d1e464a72f47169a2e90
Moshe Avni <mavni@netapp.com>
2024-08-08 23:09:22 +0300

    add lazyupdate autocmd

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -229,3 +229,43 @@ autocmd('BufWritePost', {
   end,
   once = false,
 })
+
+-- Automatically commit lockfile after running Lazy Update (or Sync)
+autocmd('User', {
+  pattern = 'LazyUpdate',
+  callback = function()
+    local repo_dir = vim.env.HOME .. '/Repos/dotfiles'
+    if vim.fn.isdirectory(repo_dir) ~= 1 then
+      return
+    end
+
+    local lockfile = repo_dir .. '/.config/nvim/lazy-lock.json'
+
+    local cmd = {
+      'git',
+      '-C',
+      repo_dir,
+      'commit',
+      lockfile,
+      '-m',
+      'Update lazy-lock.json',
+    }
+
+    local success, process = pcall(function()
+      return vim.system(cmd):wait()
+    end)
+
+    if process and process.code == 0 then
+      vim.notify 'Committed lazy-lock.json'
+      vim.notify(process.stdout)
+    else
+      if not success then
+        vim.notify("Failed to run command '" .. table.concat(cmd, ' ') .. "':", vim.log.levels.WARN, {})
+        vim.notify(tostring(process), vim.log.levels.WARN, {})
+      else
+        vim.notify 'git ran but failed to commit:'
+        vim.notify(process.stderr, vim.log.levels.WARN, {})
+      end
+    end
+  end,
+})
