c765f180627ff94ea103c2023007fcbbdf9a6a82
Moshe Avni <mavni@netapp.com>
2024-08-21 22:27:39 +0300

    kubectl

--- .config/nvim/lua/plugins/kubectl.lua
+++ .config/nvim/lua/plugins/kubectl.lua
@@ -1,48 +1,19 @@
 return {
-    dir = '/Users/mavni/Repos/kubectl.nvim',
-    -- 'mosheavni/kubectl.nvim',
-    opts = {
-      diff = { bin = 'kdiff' },
-      custom_views = {
-        ['externalsecrets.external-secrets.io'] = {
-          resource = 'externalsecrets',
-          display_name = 'ExternalSecrets',
-          ft = 'k8s_externalsecret',
-          -- url = { '{{BASE}}/apis/external-secrets.io/v1beta1/externalsecrets' },
-          -- cmd = 'curl',
-          headers = {
-            { name = 'NAMESPACE', func = false }, -- part of fallback view, no need to recalculate
-            { name = 'NAME', func = false }, -- part of fallback view, no need to recalculate
-            {
-              name = 'STORE',
-              func = function(row)
-                return row.spec.secretStoreRef.name
-              end,
-            },
-            {
-              name = 'REFRESH_INTERVAL',
-              func = function(row)
-                return row.spec.refreshInterval
-              end,
-            },
-            {
-              name = 'STATUS',
-              func = function(row)
-                return row.status.conditions[1].reason
-              end,
-            },
-            {
-              name = 'READY',
-              func = function(row)
-                return row.status.conditions[1].type
-              end,
-            },
-            { name = 'AGE', func = false }, -- part of fallback view, no need to recalculate
-          },
-        },
-      },
+  dir = '/Users/mavni/Repos/kubectl.nvim',
+  -- 'mosheavni/kubectl.nvim',
+  opts = {
+    diff = { bin = 'kdiff' },
+    -- logging = {
+    --   level = 'trace',
+    -- },
+    notifications = {
+      enabled = true,
+      verbose = true,
+      blend = 0,
     },
-    keys = {
-      { '<leader>k', '<cmd>lua require("kubectl").open()<cr>' },
-    },
-  }
+  },
+  cmd = { 'Kubectl', 'Kubectx', 'Kubens' },
+  keys = {
+    { '<leader>k', '<cmd>lua require("kubectl").toggle()<cr>' },
+  },
+}
