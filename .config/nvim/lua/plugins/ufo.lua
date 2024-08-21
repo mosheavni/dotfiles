f94a8ed560141d3541aa121bc37eed89addce446
Moshe Avni <mavni@netapp.com>
2024-08-21 22:27:48 +0300

    revert ufo

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -1,35 +1,3 @@
-local handler = function(virtText, lnum, endLnum, width, truncate)
-  local newVirtText = {}
-  local _end = endLnum - 1
-  local final_text = vim.trim(vim.api.nvim_buf_get_text(0, _end, 0, _end, -1, {})[1])
-  local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-  local sufWidth = vim.fn.strdisplaywidth(suffix .. final_text .. '...')
-  local targetWidth = width - sufWidth
-  local curWidth = 0
-  for _, chunk in ipairs(virtText) do
-    local chunkText = chunk[1]
-    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-    if targetWidth > curWidth + chunkWidth then
-      table.insert(newVirtText, chunk)
-    else
-      chunkText = truncate(chunkText, targetWidth - curWidth)
-      local hlGroup = chunk[2]
-      table.insert(newVirtText, { chunkText, hlGroup })
-      chunkWidth = vim.fn.strdisplaywidth(chunkText)
-      -- str width returned from truncate() may less than 2nd argument, need padding
-      if curWidth + chunkWidth < targetWidth then
-        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-      end
-      break
-    end
-    curWidth = curWidth + chunkWidth
-  end
-  table.insert(newVirtText, { ' ⋯ ' })
-  table.insert(newVirtText, { final_text, 'UfoFoldedFg' })
-  table.insert(newVirtText, { suffix, 'MoreMsg' })
-  return newVirtText
-end
-
 local M = {
   'kevinhwang91/nvim-ufo',
   dependencies = { 'kevinhwang91/promise-async' },
@@ -49,7 +17,6 @@ local M = {
     provider_selector = function()
       return { 'treesitter', 'indent' }
     end,
-    fold_virt_text_handler = handler,
   },
 
   init = function()
