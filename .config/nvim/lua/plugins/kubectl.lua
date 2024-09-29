c95a36e59a18e481ae6e8073c1ff3ae2b02a8666
Moshe Avni <mavni@netapp.com>
2024-09-29 14:48:48 +0300

    add kubectl mappings

--- .config/nvim/lua/plugins/kubectl.lua
+++ .config/nvim/lua/plugins/kubectl.lua
@@ -10,6 +10,7 @@ return {
     --   'default',
     --   'kube-system',
     -- },
+    context = true,
     filter = {
       apply_on_select_from_history = false,
     },
@@ -18,5 +19,8 @@ return {
   keys = {
     { '<leader>k', '<cmd>lua require("kubectl").toggle()<cr>' },
     { '<C-k>', '<Plug>(kubectl.kill)', ft = 'k8s_*' },
+    { '7', '<Plug>(kubectl.view_nodes)', ft = 'k8s_*' },
+    { '8', '<Plug>(kubectl.view_overview)', ft = 'k8s_*' },
+    { '<C-t>', '<Plug>(kubectl.view_top)', ft = 'k8s_*' },
   },
 }
