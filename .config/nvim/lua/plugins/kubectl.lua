503fcdcf5143f2d677e2aeda965cca523fd84257
Moshe Avni <mavni@netapp.com>
2024-10-01 00:11:03 +0300

    toggle faults

--- .config/nvim/lua/plugins/kubectl.lua
+++ .config/nvim/lua/plugins/kubectl.lua
@@ -1,15 +1,9 @@
 return {
   dir = '/Users/mavni/Repos/kubectl.nvim',
-  -- 'mosheavni/kubectl.nvim',
+  -- 'Ramilito/kubectl.nvim',
   opts = {
+    log_level = vim.log.levels.DEBUG,
     diff = { bin = 'kdiff' },
-    -- logging = {
-    --   level = 'trace',
-    -- },
-    -- namespace_fallback = {
-    --   'default',
-    --   'kube-system',
-    -- },
     context = true,
     filter = {
       apply_on_select_from_history = false,
@@ -22,5 +16,19 @@ return {
     { '7', '<Plug>(kubectl.view_nodes)', ft = 'k8s_*' },
     { '8', '<Plug>(kubectl.view_overview)', ft = 'k8s_*' },
     { '<C-t>', '<Plug>(kubectl.view_top)', ft = 'k8s_*' },
+    {
+      'Z',
+      function()
+        local state = require 'kubectl.state'
+        local current = state.getFilter()
+        local faults_filter = '!1/1,!2/2,!3/3,!4/4,!5/5,!6/6,!7/7,!Completed,!Terminating'
+        if current == faults_filter then
+          state.setFilter ''
+          return
+        end
+        state.setFilter(faults_filter)
+      end,
+      desc = 'Toggle faults',
+    },
   },
 }
