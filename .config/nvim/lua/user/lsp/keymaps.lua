14bca2a9ae8d35f0a3409f77842cb0d921971b6a
Moshe Avni <mavni@netapp.com>
2024-10-10 20:25:30 +0300

    lsp keymaps

--- .config/nvim/lua/user/lsp/keymaps.lua
+++ .config/nvim/lua/user/lsp/keymaps.lua
@@ -4,24 +4,11 @@ return function(bufnr)
   end
 
   -- GoTo code navigation
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, returnOpts 'Go to declaration')
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, returnOpts 'Go to definition')
   vim.keymap.set('n', 'gp', '<cmd>Lspsaga peek_definition<CR>', returnOpts 'Peek definition')
   vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, returnOpts 'Go to type definition')
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, returnOpts 'Go to implementation')
   vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.references({ includeDeclaration = false })<CR>', returnOpts 'Go to references (native)')
   vim.keymap.set('n', 'grR', '<cmd>Lspsaga finder<CR>', returnOpts 'Go to references')
-
-  -- Documentation
-  vim.keymap.set('i', '<M-k>', vim.lsp.buf.signature_help, returnOpts 'Signature help')
-  vim.keymap.set('n', '<leader>lk', vim.lsp.buf.signature_help, returnOpts 'Signature help')
-  -- calling twice make the cursor go into the float window. good for navigating big docs
-  -- vim.keymap.set('n','K', vim.lsp.buf.hover, returnOpts 'Hover doc')
-  vim.keymap.set('n', 'K', ':Lspsaga hover_doc<CR>', returnOpts 'Hover doc')
-
-  -- Refactor rename
-  -- vim.keymap.set('n','<leader>lrn', vim.lsp.buf.rename, returnOpts 'Rename')
-  vim.keymap.set('n', '<leader>lrn', ':Lspsaga rename ++project<CR>', returnOpts 'Rename')
 
   -- Workspace
   vim.keymap.set('n', '<leader>lwa', vim.lsp.buf.add_workspace_folder, returnOpts 'Add workspace folder')
@@ -36,7 +23,7 @@ return function(bufnr)
   end, returnOpts 'Toggle inlay hints')
 
   -- Diagnostics
-  vim.keymap.set('n', '<leader>lq', vim.diagnostic.setqflist, returnOpts 'Set qflist')
+  vim.keymap.set('n', '<leader>lq', vim.diagnostic.setqflist, returnOpts 'Set qflist with diagnostics')
   vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, returnOpts 'Open diagnostics float window')
 
   -- Code action
@@ -44,5 +31,5 @@ return function(bufnr)
   vim.keymap.set('n', '<leader>lx', vim.lsp.codelens.run, returnOpts 'Code lens')
 
   -- code outline
-  vim.keymap.set('n', '<leader>o', ':Lspsaga outline<CR>', returnOpts 'Code lens')
+  vim.keymap.set('n', '<leader>o', ':Lspsaga outline<CR>', returnOpts 'Code outline')
 end
