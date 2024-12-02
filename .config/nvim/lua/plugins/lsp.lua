dbf1edc0b5546dffef8a66db6ffdf66052b41271
Moshe Avni <mavni@netapp.com>
2024-12-02 11:36:55 +0200

    remove go

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -115,20 +115,6 @@ local language_specific_plugins = {
     --   require('cmp').register_source('lazydev', )
     -- end
   },
-  {
-    'ray-x/go.nvim',
-    dependencies = { -- optional packages
-      'ray-x/guihua.lua',
-      'neovim/nvim-lspconfig',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    config = function()
-      require('go').setup()
-    end,
-    event = { 'CmdlineEnter' },
-    ft = { 'go', 'gomod' },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
 }
 
 return {
