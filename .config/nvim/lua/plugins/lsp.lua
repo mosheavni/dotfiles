307621e7ccefee547ebdc974c20928e3760cf896
Moshe Avni <mavni@netapp.com>
2024-12-14 13:32:13 +0200

    add dropbar

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -34,6 +34,26 @@ M.dependencies = {
   {
     'Bekaboo/dropbar.nvim',
     config = function()
+      require('dropbar').setup {
+        bar = {
+          sources = function(buf, _)
+            local sources = require 'dropbar.sources'
+            local utils = require 'dropbar.utils'
+            if vim.bo[buf].ft == 'markdown' then
+              return { sources.markdown }
+            end
+            if vim.bo[buf].buftype == 'terminal' then
+              return { sources.terminal }
+            end
+            return {
+              utils.source.fallback {
+                sources.lsp,
+                sources.treesitter,
+              },
+            }
+          end,
+        },
+      }
       local dropbar_api = require 'dropbar.api'
       vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
       vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
