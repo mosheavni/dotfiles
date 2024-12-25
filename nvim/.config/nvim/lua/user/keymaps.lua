682d1e61172bd3deedabf67e9ed0846725a1acf2
Moshe Avni <mavni@netapp.com>
2024-12-25 22:30:48 +0200

    finish diffwithsaved

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -350,22 +350,9 @@ vim.api.nvim_create_user_command('DiffWithSaved', function()
   -- Get start buffer
   local start = vim.api.nvim_get_current_buf()
   local filetype = vim.api.nvim_get_option_value('filetype', { buf = start })
-
-  -- `vnew` - Create empty vertical split window
-  -- `set buftype=nofile` - Buffer is not related to a file, will not be written
-  -- `read ++edit #` - Read the current file into the new buffer
-  -- `0d_` - Remove an extra empty start row
-  -- `diffthis` - Set diff mode to a new vertical split
   vim.cmd 'vnew | set buftype=nofile | read ++edit # | 0d_ | diffthis'
-
-  -- Get scratch buffer
   local scratch = vim.api.nvim_get_current_buf()
-
-  -- Set filetype of scratch buffer to be the same as start
   vim.api.nvim_set_option_value('filetype', filetype, { buf = scratch })
-
-  -- `wincmd p` - Go to the start window
-  -- `diffthis` - Set diff mode to a start window
   vim.cmd 'wincmd p | diffthis'
 
   -- Map `q` for both buffers to exit diff view and delete scratch buffer
