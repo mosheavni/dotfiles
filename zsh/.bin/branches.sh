9b983e08272b4cc725d0d93dbec9a05c279e9a08
Moshe Avni <mavni@netapp.com>
2025-01-13 15:57:04 +0200

    branches wip

--- zsh/.bin/branches.sh
+++ zsh/.bin/branches.sh
@@ -1,32 +1,30 @@
-#!/bin/bash
+#!/bin/zsh
 
-current_branch=$(git branch --show-current)
+setopt LOCAL_OPTIONS PIPE_FAIL ERR_EXIT
+
+current_branch=$(git symbolic-ref --short HEAD)
 
 print_remote_or_local() {
-  branch_name=$1
-  branch_name_no_remotes=$(sed -E 's/remotes\/[^\/]*\///' <<<"$branch_name")
+  local branch_name=$1
+  local branch_name_no_remotes=${branch_name#remotes/origin/}
   [[ "$branch_name_no_remotes" == "$current_branch" ]] && return
-  if [[ $branch_name == *"remotes/"* ]]; then
-    # if ! grep -q "$branch_name_no_remotes" <<<"$local_branches"; then
-    #   echo -e "\033[31m$branch_name\033[0m"
-    # fi
-    [[ ! $local_branches =~ $branch_name_no_remotes ]] &&
-      echo -e "  \033[31m$branch_name\033[0m"
+
+  if [[ $branch_name == remotes/* ]]; then
+    if [[ ! ${local_branches[(r)$branch_name_no_remotes]} ]]; then
+      print -P "%F{red}  ${branch_name}%f"
+    fi
   else
-    echo "  $branch_name"
+    print "  $branch_name"
   fi
 }
-export -f print_remote_or_local
 
-all_other_branches=$(git branch --all --sort=-committerdate | grep -v "^\*\|HEAD" | tr -d " ")
-local_branches=$(git branch --format="%(refname:short)")
-first_branch=$(head -n1 <<<"$all_other_branches")
-remaining_branches=$(tail -n +2 <<<"$all_other_branches")
+all_branches=("${(@f)$(git branch --all --sort=-committerdate | grep -v "^\*\|HEAD" | tr -d " ")}")
+local_branches=("${(@f)$(git branch --format="%(refname:short)")}")
 
-# Print in desired order
-print_remote_or_local "$first_branch"
-echo -e "\033[32m*\033[0m $current_branch"
+print_remote_or_local $all_branches[1]
+print -P "%F{green}*%f $current_branch"
 
-export current_branch
-export local_branches
-echo "$remaining_branches" | parallel -k print_remote_or_local
+# Process remaining branches without parallel
+for branch in "${(@)all_branches[@]:1}"; do
+  print_remote_or_local $branch
+done
