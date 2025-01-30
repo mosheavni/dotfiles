bbce1d14c20ffe7658d2def000be90d4d9424a3c
Moshe Avni <mavni@netapp.com>
2025-01-30 19:12:59 +0200

    code actions optimize

--- nvim/.config/nvim/lua/user/lsp/code-actions.lua
+++ nvim/.config/nvim/lua/user/lsp/code-actions.lua
@@ -2,6 +2,12 @@ local git = require 'user.git'
 local null_ls = require 'null-ls'
 
 local M = {}
+local api = vim.api
+
+local function get_indent(line)
+  return string.match(line, '^%s*')
+end
+
 M.revision_branch_comment = {
   method = null_ls.methods.CODE_ACTION,
   filetypes = { 'yaml' },
@@ -16,33 +22,31 @@ M.revision_branch_comment = {
       if is_argo_app then
         local target_revision_line_number = vim.fn.search('targetRevision: ', 'nw')
         local target_revision_line = context.content[target_revision_line_number]
+        local indent = get_indent(target_revision_line)
+
         if string.find(target_revision_line, 'targetRevision: HEAD') then
           return {
             {
               title = 'Change branch to current',
               action = function()
-                -- get indentation of current_line
-                local indent = string.match(target_revision_line, '^%s*')
                 local new_lines = {
                   indent .. 'targetRevision: ' .. git.get_branch_sync() .. ' # TODO: Change to HEAD before merging',
                 }
-                vim.api.nvim_buf_set_lines(context.bufnr, target_revision_line_number - 1, target_revision_line_number, false, new_lines)
-              end,
-            },
-          }
-        else
-          return {
-            {
-              title = 'Change branch to HEAD',
-              action = function()
-                -- get indentation of current_line
-                local indent = string.match(target_revision_line, '^%s*')
-                local new_lines = { indent .. 'targetRevision: HEAD' }
-                vim.api.nvim_buf_set_lines(context.bufnr, target_revision_line_number - 1, target_revision_line_number, false, new_lines)
+                api.nvim_buf_set_lines(context.bufnr, target_revision_line_number - 1, target_revision_line_number, false, new_lines)
               end,
             },
           }
         end
+
+        return {
+          {
+            title = 'Change branch to HEAD',
+            action = function()
+              local new_lines = { indent .. 'targetRevision: HEAD' }
+              api.nvim_buf_set_lines(context.bufnr, target_revision_line_number - 1, target_revision_line_number, false, new_lines)
+            end,
+          },
+        }
       end
     end,
   },
@@ -57,7 +61,7 @@ M.toggle_function_params = {
       local row = context.range.row
       local current_line = context.content[row]
       local is_function = vim.regex([=[\v\s*def\s[^\)]+\(]=]):match_str(current_line)
-      local final = current_line
+
       if is_function then
         return {
           {
@@ -66,6 +70,8 @@ M.toggle_function_params = {
               local function_args = current_line:gsub([=[%s*def%s%w+%((.*)%).*]=], '%1')
               function_args = function_args:gsub('%s?=%s?', '=')
               function_args = function_args:gsub('%s?:%s?', ':')
+              local final
+
               if string.find(function_args, 'Map args=') then
                 function_args = function_args:gsub('Map args=%[(.*)%]', '%1')
                 function_args = function_args:gsub('(%w+):([^,]*)', '%1=%2')
@@ -74,9 +80,8 @@ M.toggle_function_params = {
                 function_args = function_args:gsub('(%w+)=([^,]*)', '%1:%2')
                 final = current_line:gsub([=[(%s*def%s%w+%().*(%).*)]=], '%1Map args=[' .. function_args .. ']%2')
               end
-              -- function_args = string.gsub(function_args, [[\w+:\w+]], '')
-              -- P(function_args)
-              vim.api.nvim_buf_set_lines(context.bufnr, row - 1, row, false, { final })
+
+              api.nvim_buf_set_lines(context.bufnr, row - 1, row, false, { final })
             end,
           },
         }
@@ -98,22 +103,21 @@ M.library_current_branch = {
             {
               title = 'Remove current branch from library',
               action = function()
-                vim.api.nvim_buf_set_lines(context.bufnr, 0, 1, false, { "@Library('utils') _" })
-              end,
-            },
-          }
-        else
-          return {
-            {
-              title = 'Change library to current branch',
-              action = function()
-                local new_line = string.format("@Library('utils@%s') _ // TODO: remove library before merging", git.get_branch_sync())
-                vim.print(vim.inspect(new_line))
-                vim.api.nvim_buf_set_lines(context.bufnr, 0, 1, false, { new_line })
+                api.nvim_buf_set_lines(context.bufnr, 0, 1, false, { "@Library('utils') _" })
               end,
             },
           }
         end
+
+        return {
+          {
+            title = 'Change library to current branch',
+            action = function()
+              local new_line = string.format("@Library('utils@%s') _ // TODO: remove library before merging", git.get_branch_sync())
+              api.nvim_buf_set_lines(context.bufnr, 0, 1, false, { new_line })
+            end,
+          },
+        }
       end
     end,
   },
