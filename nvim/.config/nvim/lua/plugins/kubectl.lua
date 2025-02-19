035ce3dd97f14227b0f7e2abb47ba914d88db220
Moshe Avni <mavni@netapp.com>
2025-02-19 17:25:46 +0200

    remove <c-k> binding from kubectl plugin

--- nvim/.config/nvim/lua/plugins/kubectl.lua
+++ nvim/.config/nvim/lua/plugins/kubectl.lua
@@ -30,7 +30,7 @@ return {
   cmd = { 'Kubectl', 'Kubectx', 'Kubens' },
   keys = {
     { '<leader>k', '<cmd>lua require("kubectl").toggle()<cr>' },
-    { '<C-k>', '<Plug>(kubectl.kill)', ft = 'k8s_*' },
+    -- { '<C-k>', '<Plug>(kubectl.kill)', ft = 'k8s_*' },
     { '7', '<Plug>(kubectl.view_nodes)', ft = 'k8s_*' },
     { '8', '<Plug>(kubectl.view_overview)', ft = 'k8s_*' },
     { '<C-t>', '<Plug>(kubectl.view_top)', ft = 'k8s_*' },
