be88b81380c7b5c197462ad73f2fe42181894a04
Moshe Avni <mavni@netapp.com>
2025-02-20 12:19:13 +0200

    fix lazy floaterm

--- nvim/.config/nvim/lua/user/lazy.lua
+++ nvim/.config/nvim/lua/user/lazy.lua
@@ -12,9 +12,9 @@ return {
     ui = {
       border = 'rounded',
       custom_keys = {
-        -- open a terminal for the plugin dir
         ['<localleader>t'] = function(plugin)
-          vim.cmd('FloatermNew --cwd=' .. plugin.dir)
+          vim.fn.setreg('+', plugin.dir)
+          vim.notify('Copied path to clipboard: ' .. plugin.dir)
         end,
       },
     },
