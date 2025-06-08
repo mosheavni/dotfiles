9707195a2cc15850d86af41d477399aa39e7b2b1
Moshe Avni <moshe.avni@flexera.com>
2025-06-08 11:48:06 +0300

    remove print

--- nvim/.config/nvim/ftplugin/k8s_applications_argoproj_io.lua
+++ nvim/.config/nvim/ftplugin/k8s_applications_argoproj_io.lua
@@ -1,6 +1,5 @@
 local commands = require 'kubectl.actions.commands'
 local tables = require 'kubectl.utils.tables'
-vim.print 'in here'
 
 vim.schedule(function()
   vim.api.nvim_buf_set_keymap(0, 'n', '<Plug>(kubectl.select)', '', {
