74d032af5a0975d6c214dab2e461877376a76293
Moshe Avni <moshe.avni@flexera.com>
2025-06-19 15:03:15 +0300

    fix fzf

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,17 +1,3 @@
-local grep_tbl = {
-  multiprocess = true,
-  RIPGREP_CONFIG_PATH = vim.env.HOME .. '/.ripgreprc',
-  -- One thing I missed from Telescope was the ability to live_grep and the
-  -- run a filter on the filenames.
-  -- Ex: Find all occurrences of "enable" but only in the "plugins" directory.
-  -- With this change, I can sort of get the same behaviour in live_grep.
-  -- ex: > enable --*/plugins/*
-  -- I still find this a bit cumbersome. There's probably a better way of doing this.
-  rg_glob = true, -- enable glob parsing
-  glob_flag = '--iglob', -- case insensitive globs
-  glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
-  hidden = true,
-}
 return {
   'ibhagwan/fzf-lua',
   event = 'VeryLazy',
@@ -161,8 +147,20 @@ return {
         cwd_only = true,
         include_current_session = true,
       },
-      grep = grep_tbl,
-      live_grep = grep_tbl,
+      grep = {
+        multiprocess = true,
+        RIPGREP_CONFIG_PATH = vim.env.HOME .. '/.ripgreprc',
+        -- One thing I missed from Telescope was the ability to live_grep and the
+        -- run a filter on the filenames.
+        -- Ex: Find all occurrences of "enable" but only in the "plugins" directory.
+        -- With this change, I can sort of get the same behaviour in live_grep.
+        -- ex: > enable --*/plugins/*
+        -- I still find this a bit cumbersome. There's probably a better way of doing this.
+        rg_glob = true, -- enable glob parsing
+        glob_flag = '--iglob', -- case insensitive globs
+        glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
+        hidden = true,
+      },
       keymap = { fzf = { ['ctrl-q'] = 'select-all+accept' } },
     }
 
