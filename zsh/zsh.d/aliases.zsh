2d2d4b8ea303d03a4eef8cb433a03203ac767798
Moshe Avni <moshe.avni@flexera.com>
2025-07-07 11:47:14 +0300

    minimal vim

--- zsh/zsh.d/aliases.zsh
+++ zsh/zsh.d/aliases.zsh
@@ -30,6 +30,7 @@ alias vi='nvim'
 alias sudoedit="nvim"
 alias lvim='NVIM_APPNAME=LazyVim nvim'
 alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc'
+alias lv='nvim -nu ${HOME}/.dotfiles/nvim/.config/nvim/minimal_init.lua '
 
 #------------------------------------------------------------------------------
 # Development Tools
