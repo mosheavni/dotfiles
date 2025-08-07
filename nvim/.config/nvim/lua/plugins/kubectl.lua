b0fb40d195194b4a32f07dfced6e28d8b351619e
Moshe Avni <moshe.avni@flexera.com>
2025-08-07 13:58:32 +0300

    add kubectl maps

--- nvim/.config/nvim/lua/plugins/kubectl.lua
+++ nvim/.config/nvim/lua/plugins/kubectl.lua
@@ -47,9 +47,9 @@ return {
   cmd = { 'Kubectl', 'Kubectx', 'Kubens' },
   keys = {
     { '<leader>k', '<cmd>lua require("kubectl").toggle()<cr>' },
-    -- { '<C-k>', '<Plug>(kubectl.kill)', ft = 'k8s_*' },
     { '7', '<Plug>(kubectl.view_nodes)', ft = 'k8s_*' },
-    { '8', '<Plug>(kubectl.view_overview)', ft = 'k8s_*' },
+    { '8', '<Plug>(kubectl.view_daemonsets)', ft = 'k8s_*' },
+    { '9', '<Plug>(kubectl.view_statefulsets)', ft = 'k8s_*' },
     { '<C-t>', '<Plug>(kubectl.view_top)', ft = 'k8s_*' },
     {
       'Z',
