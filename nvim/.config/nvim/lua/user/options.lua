f78f1c6b9edab69d136ee0606775f63c7b74ca59
Moshe Avni <moshe.avni@flexera.com>
2025-11-12 16:13:57 +0200

    add extui, may delete later

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -228,3 +228,16 @@ vim.filetype.add {
     end,
   },
 }
+
+-----------
+-- EXTUI --
+-----------
+require('vim._extui').enable {
+  enable = true, -- Whether to enable or disable the UI.
+  msg = { -- Options related to the message module.
+    ---@type 'cmd'|'msg' Where to place regular messages, either in the
+    ---cmdline or in a separate ephemeral message window.
+    target = 'cmd',
+    timeout = 4000, -- Time a message is visible in the message window.
+  },
+}
