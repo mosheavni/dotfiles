74a3703f0a770e24cca6bf6ae0173f44c41f09f3
Moshe Avni <moshe.avni@flexera.com>
2025-11-12 12:20:32 +0200

    add dirdiff cmd

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -492,3 +492,22 @@ vim.cmd('source ' .. vim.fn.stdpath 'config' .. '/lua/user/search-replace.vim')
 require('user.tabular-v2').setup {}
 require('user.projects').setup()
 require 'user.number-separators'
+
+--------------
+-- Difftool --
+--------------
+vim.api.nvim_create_user_command('DirDiff', function(opts)
+  if vim.tbl_count(opts.fargs) ~= 2 then
+    vim.notify('DirDiff requires exactly two directory arguments', vim.log.levels.ERROR)
+    return
+  end
+
+  vim.cmd 'tabnew'
+  vim.cmd.packadd 'nvim.difftool'
+  require('difftool').open(opts.fargs[1], opts.fargs[2], {
+    rename = {
+      detect = false,
+    },
+    ignore = { '.git' },
+  })
+end, { complete = 'dir', nargs = '*' })
