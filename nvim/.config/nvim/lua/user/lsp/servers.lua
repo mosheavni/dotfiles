ee9d3e863e6bb6d43942ea56e190bc80955810d8
Moshe Avni <moshe.avni@flexera.com>
2025-11-13 13:35:38 +0200

    fix lua_ls

--- nvim/.config/nvim/lua/user/lsp/servers.lua
+++ nvim/.config/nvim/lua/user/lsp/servers.lua
@@ -45,6 +45,16 @@ M.setup = function()
   })
 
   vim.lsp.config('lua_ls', {
+    root_markers = {
+      '.luarc.json',
+      '.luarc.jsonc',
+      '.luacheckrc',
+      '.stylua.toml',
+      'stylua.toml',
+      'selene.toml',
+      'selene.yml',
+    },
+
     settings = {
       Lua = {
         runtime = { version = 'LuaJIT' },
