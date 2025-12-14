49330fd61701375542c9defb518d9eadcce1407c
Moshe Avni <moshe.avni@flexera.com>
2025-12-14 13:10:50 +0200

    go lazy

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -169,7 +169,6 @@ local language_specific_plugins = {
         group = format_sync_grp,
       })
     end,
-    event = { 'CmdlineEnter' },
     ft = { 'go', 'gomod' },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
