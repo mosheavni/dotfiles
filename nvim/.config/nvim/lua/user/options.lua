73a07a9eecbbdb0fae6d91d456cc546fff4bec3c
Moshe Avni <moshe.avni@flexera.com>
2025-12-17 10:32:45 +0200

    extui enabled

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -236,12 +236,4 @@ require('user.input').setup()
 -----------
 -- EXTUI --
 -----------
--- require('vim._extui').enable {
---   enable = true, -- Whether to enable or disable the UI.
---   msg = { -- Options related to the message module.
---     ---@type 'cmd'|'msg' Where to place regular messages, either in the
---     ---cmdline or in a separate ephemeral message window.
---     target = 'cmd',
---     timeout = 4000, -- Time a message is visible in the message window.
---   },
--- }
+require('vim._extui').enable { enable = true, msg = { target = 'cmd' } }
