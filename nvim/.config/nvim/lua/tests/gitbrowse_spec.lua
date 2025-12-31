070f016f56c3a2313525ccf420f8dca22e32d0a4
Moshe Avni <moshe.avni@flexera.com>
2025-12-31 14:14:52 +0200

    fix tests

--- nvim/.config/nvim/lua/tests/gitbrowse_spec.lua
+++ nvim/.config/nvim/lua/tests/gitbrowse_spec.lua
@@ -57,41 +57,6 @@ describe('user.gitbrowse', function()
   end)
 
   describe('get_url', function()
-    local default_opts = {
-      what = 'file',
-      remote_patterns = {
-        { '^(https?://.*)%.git$', '%1' },
-        { '^git@(.+):(.+)%.git$', 'https://%1/%2' },
-        { '^git@(.+):(.+)$', 'https://%1/%2' },
-        { '^git@(.+)/(.+)$', 'https://%1/%2' },
-        { '^ssh://git@(.*)$', 'https://%1' },
-        { '^ssh://([^:/]+)(:%d+)/(.*)$', 'https://%1/%3' },
-        { '^ssh://([^/]+)/(.*)$', 'https://%1/%2' },
-        { 'ssh%.dev%.azure%.com/v3/(.*)/(.*)$', 'dev.azure.com/%1/_git/%2' },
-        { '^https://%w*@(.*)', 'https://%1' },
-        { '^git@(.*)', 'https://%1' },
-        { ':%d+', '' },
-        { '%.git$', '' },
-      },
-      url_patterns = {
-        ['github%.com'] = {
-          branch = '/tree/{branch}',
-          file = '/blob/{branch}/{file}#L{line_start}-L{line_end}',
-          commit = '/commit/{commit}',
-        },
-        ['gitlab%.com'] = {
-          branch = '/-/tree/{branch}',
-          file = '/-/blob/{branch}/{file}#L{line_start}-L{line_end}',
-          commit = '/-/commit/{commit}',
-        },
-        ['bitbucket%.org'] = {
-          branch = '/src/{branch}',
-          file = '/src/{branch}/{file}#lines-{line_start}-L{line_end}',
-          commit = '/commits/{commit}',
-        },
-      },
-    }
-
     it('builds GitHub file URL', function()
       local repo = 'https://github.com/user/repo'
       local fields = {
@@ -100,23 +65,21 @@ describe('user.gitbrowse', function()
         line_start = 10,
         line_end = 20,
       }
-      local url = gitbrowse.get_url(repo, fields, default_opts)
+      local url = gitbrowse.get_url(repo, fields)
       eq(url, 'https://github.com/user/repo/blob/main/src/test.lua#L10-L20')
     end)
 
     it('builds GitHub branch URL', function()
       local repo = 'https://github.com/user/repo'
       local fields = { branch = 'develop' }
-      local opts = vim.tbl_extend('force', default_opts, { what = 'branch' })
-      local url = gitbrowse.get_url(repo, fields, opts)
+      local url = gitbrowse.get_url(repo, fields, { what = 'branch' })
       eq(url, 'https://github.com/user/repo/tree/develop')
     end)
 
     it('builds GitHub commit URL', function()
       local repo = 'https://github.com/user/repo'
       local fields = { commit = 'abc123def456' }
-      local opts = vim.tbl_extend('force', default_opts, { what = 'commit' })
-      local url = gitbrowse.get_url(repo, fields, opts)
+      local url = gitbrowse.get_url(repo, fields, { what = 'commit' })
       eq(url, 'https://github.com/user/repo/commit/abc123def456')
     end)
 
@@ -128,15 +91,14 @@ describe('user.gitbrowse', function()
         line_start = 5,
         line_end = 15,
       }
-      local url = gitbrowse.get_url(repo, fields, default_opts)
+      local url = gitbrowse.get_url(repo, fields)
       eq(url, 'https://gitlab.com/user/repo/-/blob/main/src/test.lua#L5-L15')
     end)
 
     it('builds GitLab branch URL', function()
       local repo = 'https://gitlab.com/user/repo'
       local fields = { branch = 'feature/test' }
-      local opts = vim.tbl_extend('force', default_opts, { what = 'branch' })
-      local url = gitbrowse.get_url(repo, fields, opts)
+      local url = gitbrowse.get_url(repo, fields, { what = 'branch' })
       eq(url, 'https://gitlab.com/user/repo/-/tree/feature/test')
     end)
 
@@ -148,14 +110,14 @@ describe('user.gitbrowse', function()
         line_start = 1,
         line_end = 10,
       }
-      local url = gitbrowse.get_url(repo, fields, default_opts)
+      local url = gitbrowse.get_url(repo, fields)
       eq(url, 'https://bitbucket.org/user/repo/src/main/src/test.lua#lines-1-L10')
     end)
 
     it('returns repo URL for unknown remote', function()
       local repo = 'https://unknown.com/user/repo'
       local fields = { branch = 'main' }
-      local url = gitbrowse.get_url(repo, fields, default_opts)
+      local url = gitbrowse.get_url(repo, fields)
       eq(url, 'https://unknown.com/user/repo')
     end)
 
@@ -167,7 +129,7 @@ describe('user.gitbrowse', function()
         line_start = 42,
         line_end = 42,
       }
-      local url = gitbrowse.get_url(repo, fields, default_opts)
+      local url = gitbrowse.get_url(repo, fields)
       eq(url, 'https://github.com/user/repo/blob/main/test.lua#L42-L42')
     end)
   end)
