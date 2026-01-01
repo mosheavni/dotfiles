98c55d73872a7e7161dc6a364b15df2c35ffca0c
Moshe Avni <moshe.avni@flexera.com>
2026-01-01 15:02:05 +0200

    random setopt

--- zsh/zsh.d/zsh.zsh
+++ zsh/zsh.d/zsh.zsh
@@ -3,16 +3,18 @@ setopt menu_complete
 unsetopt auto_menu
 unsetopt case_glob
 setopt glob_complete
-setopt multios             # enable redirect to multiple streams: echo >file1 >file2
-setopt long_list_jobs      # show long list format job notifications
-setopt interactivecomments # recognize comments
-setopt autocd
+setopt multi_os             # enable redirect to multiple streams: echo >file1 >file2
+setopt long_list_jobs       # show long list format job notifications
+setopt interactive_comments # recognize comments
+setopt auto_cd
 setopt complete_in_word
+setopt notify
+setopt numeric_glob_sort # sort numerically when possible (file1 file2 file10)
 
 bindkey '^q' push-line
 
 copy-line-to-clipboard() {
-    echo -n "$BUFFER" | pbcopy
+  echo -n "$BUFFER" | pbcopy
 }
 zle -N copy-line-to-clipboard
 bindkey '^Y' copy-line-to-clipboard
