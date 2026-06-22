cc7916b7b67122db1e8cf2dd7f557da564d3990c
Moshe Avni <moshe.av@appcharge.com>
2026-06-22 14:17:19 +0300

    🌍 work in progress Mon Jun 22 14:17:19 2026

--- nvim/.config/nvim/lua/user/run-buffer/handlers/lua.lua
+++ nvim/.config/nvim/lua/user/run-buffer/handlers/lua.lua
@@ -3,26 +3,19 @@ local M = {}
 
 M.ft = 'lua'
 
---- Reload a Neovim lua module under `nvim/lua/` via `:luafile %`.
---- Clears `package.loaded` for the dotted module path when applicable.
----@param file_name string Absolute path to the `.lua` file.
-local function run_lua(file_name)
-  local path = file_name:match 'nvim/lua/(.*)%.lua'
-  if path then
-    path = path:gsub('/', '.')
-    if package.loaded[path] then
-      package.loaded[path] = nil
-      vim.notify('Unloaded package.path: ' .. path, vim.log.levels.INFO)
-    end
-  end
-  vim.cmd 'luafile %'
-  vim.notify('Reloading lua file', vim.log.levels.INFO)
-end
-
 ---@type RunHandler
 M.handler = {
   resolve = function(ctx)
-    run_lua(ctx.file_name)
+    local path = ctx.file_name:match 'nvim/lua/(.*)%.lua'
+    if path then
+      path = path:gsub('/', '.')
+      if package.loaded[path] then
+        package.loaded[path] = nil
+        vim.notify('Unloaded package.path: ' .. path, vim.log.levels.INFO)
+      end
+    end
+    vim.cmd 'luafile %'
+    vim.notify('Reloading lua file', vim.log.levels.INFO)
     return { spawn = false }
   end,
 }
--- nvim/.config/nvim/lua/user/run-buffer/resolve.lua
+++ nvim/.config/nvim/lua/user/run-buffer/resolve.lua
@@ -9,33 +9,6 @@ local append_file = {
   terraform = false,
 }
 
---- Default resolve: `command_for_filetype` + file path, or shebang path only.
----@param ctx RunContext
----@return RunResult
-local function default_resolve(ctx)
-  local cmd
-  if ctx.first_line:match '^#!' then
-    cmd = ctx.file_name
-  else
-    cmd = utils.command_for_filetype(ctx.ft)
-    if append_file[ctx.ft] ~= false then
-      cmd = cmd .. ' ' .. ctx.file_name
-    end
-  end
-  return { cmd = cmd, spawn = true }
-end
-
---- Dispatch to a registered handler or the default builder.
----@param ctx RunContext
----@return RunResult
-local function invoke_resolve(ctx)
-  local h = handlers[ctx.ft]
-  if h and h.resolve then
-    return h.resolve(ctx)
-  end
-  return default_resolve(ctx)
-end
-
 local M = {}
 
 --- Register a handler from a `{ ft, handler }` module.
@@ -61,11 +34,25 @@ end
 ---@param file_name string Absolute path to run.
 ---@return RunResult
 function M.run(ft, file_name)
-  return invoke_resolve {
+  local ctx = {
     ft = ft,
     file_name = file_name,
     first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] or '',
   }
+  local h = handlers[ctx.ft]
+  if h and h.resolve then
+    return h.resolve(ctx)
+  end
+  local cmd
+  if ctx.first_line:match '^#!' then
+    cmd = ctx.file_name
+  else
+    cmd = utils.command_for_filetype(ctx.ft)
+    if append_file[ctx.ft] ~= false then
+      cmd = cmd .. ' ' .. ctx.file_name
+    end
+  end
+  return { cmd = cmd, spawn = true }
 end
 
 require('user.run-buffer.handlers').register_all(M)
