7c89be7efa1d61598acd4d7162ada3db84e7b6a4
Moshe Avni <moshe.av@appcharge.com>
2026-07-06 13:57:31 +0300

    terraform -> tg handler

--- nvim/.config/nvim/lua/user/run-buffer/handlers/terraform.lua
+++ nvim/.config/nvim/lua/user/run-buffer/handlers/terraform.lua
@@ -1,11 +1,16 @@
 -- Terraform: run plan in the buffer directory (no file path appended).
+-- A terragrunt.hcl next to the buffer means the directory is terragrunt-managed.
 local utils = require 'user.utils'
 
 return {
   ft = 'terraform',
   ---@type RunHandler
   handler = {
-    resolve = function()
+    resolve = function(ctx)
+      local dir = vim.fs.dirname(ctx.file_name)
+      if vim.uv.fs_stat(dir .. '/terragrunt.hcl') then
+        return { cmd = 'terragrunt plan', spawn = true }
+      end
       return { cmd = utils.command_for_filetype 'terraform', spawn = true }
     end,
   },
