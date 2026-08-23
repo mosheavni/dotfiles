a39d521aa8e7ff454da28d360609858e1b47789f
Moshe Avni <moshe1993@gmail.com>
2026-08-23 16:24:30 +0300

    load spellfile plugin

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -18,7 +18,6 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_remote_plugins = 1
 vim.g.loaded_shada_autoload = 1
 vim.g.loaded_shada_plugin = 1
-vim.g.loaded_spellfile_plugin = 1
 vim.g.loaded_tar = 1
 vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
