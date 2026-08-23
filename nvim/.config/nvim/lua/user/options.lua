8380f5a6a81c029b86462acd63f8fd3491eb891e
Moshe Avni <moshe1993@gmail.com>
2026-08-23 16:53:45 +0300

    don't load dir plugin

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -15,6 +15,7 @@ vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_nvim_dir_plugin = 1
 vim.g.loaded_remote_plugins = 1
 vim.g.loaded_shada_autoload = 1
 vim.g.loaded_shada_plugin = 1
